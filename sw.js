da9cd073987a463377263ce3a235e3546559ad74
umairahsuleiman17-Zara <umairahsuleiman17@gmail.com>
2026-07-16 23:47:19 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,5 @@
-const CACHE='rara-premium-v2';
+const CACHE='rara-gold-expansion-v3';
 const CORE=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
-});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([
-  self.clients.claim(),
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-])));
-self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){
-    e.respondWith(
-      fetch(e.request).then(r=>{
-        const copy=r.clone();
-        caches.open(CACHE).then(c=>c.put(e.request,copy));
-        return r;
-      }).catch(()=>caches.match('./index.html'))
-    );
-  }else{
-    e.respondWith(
-      caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{
-        const copy=n.clone();
-        caches.open(CACHE).then(c=>c.put(e.request,copy));
-        return n;
-      }))
-    );
-  }
-});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match('./index.html')))}else{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))}});
