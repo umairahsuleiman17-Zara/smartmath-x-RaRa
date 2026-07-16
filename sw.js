6dc9aa3671a78aa3e76152c30a5559e56c38d968
umairahsuleiman17-Zara <umairahsuleiman17@gmail.com>
2026-07-17 02:23:15 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,4 @@
-const CACHE='rara-family-gold-v4-2';
-const CORE=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
-});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([
-  self.clients.claim(),
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-])));
-self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
-      const copy=r.clone();
-      caches.open(CACHE).then(c=>c.put('./index.html',copy));
-      return r;
-    }).catch(()=>caches.match('./index.html')));
-  }else{
-    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-  }
-});
+const CACHE='rara-quality-fix-v4-3';const CORE=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));else e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
