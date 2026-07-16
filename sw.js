4481c61ccf0337809f7effa1afc3e420c78548a4
umairahsuleiman17-Zara <umairahsuleiman17@gmail.com>
2026-07-16 23:13:19 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE='rara-premium-v2';
+const CORE=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
+});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+])));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(
+      fetch(e.request).then(r=>{
+        const copy=r.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
+        return r;
+      }).catch(()=>caches.match('./index.html'))
+    );
+  }else{
+    e.respondWith(
+      caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{
+        const copy=n.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy));
+        return n;
+      }))
+    );
+  }
+});
